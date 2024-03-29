64ccc1273efdd49f47afe42da8151756680f5c68
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2024-03-29 19:15:02 +0530

    adddd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,24 @@ import CreateJobs from "./pages/CreateJobs";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import theme from "./Theme";
-import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, Container, CssBaseline, ThemeProvider } from "@mui/material";
 import ConnectUs from "./pages/ConnectUs";
-
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Navbar />
-      <Box sx={{backgroundColor:'primary.background'}}>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/find-jobs" element={<FindJobs />}></Route>
-          <Route path="/create-jobs" element={<CreateJobs />}></Route>
-          <Route path="/about-us" element={<AboutUs />}></Route>
-          <Route path="/connect-us" element={<ConnectUs />}></Route>
-        </Routes>
+      <Box sx={{ backgroundColor: "primary.background" }}>
+        <Container maxWidth='xl' sx={{paddingY:5 }}  >
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/find-jobs" element={<FindJobs />}></Route>
+            <Route path="/create-jobs" element={<CreateJobs />}></Route>
+            <Route path="/about-us" element={<AboutUs />}></Route>
+            <Route path="/connect-us" element={<ConnectUs />}></Route>
+          </Routes>
+        </Container>
       </Box>
     </ThemeProvider>
   );
